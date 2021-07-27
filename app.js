a4dedcf6450cb440cf744c9172125cbf4f903dd6
itsjustrobert <robertlove99999@gmail.com>
2021-07-27 11:10:39 -0500

    contined this rendition of the cookie project

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('your insanity test is passed');
+// console.log('your insanity test is passed');
+
+// sift throught the requirements.
+alert("loaded your js")
+// make the first object
+let Seattle={
+// identify some properties
+minimumCustomers:23,
+maxCustomers:65,
+averageCookiesSold:6.3
+}
